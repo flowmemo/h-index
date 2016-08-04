b2a5dac8432fb17977b01d0db02616cade50d2d7
flowmemo <flowmemo@outlook.com>
2016-08-05 00:28:58 +0800

    add doc

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 'use strict'
-function hindex (list) {
-  list = list.sort((a, b) => b - a)
-  const len = list.length
+/**
+ * @param {number[]} nums - The input array of number.
+ * @return {{h: number, i10: number, sum: number}}
+ *         The return object containning h-index, i10-index, and sum of nums.
+ */
+function hindex (nums) {
+  nums = nums.sort((a, b) => b - a)
+  const len = nums.length
   let sum = 0
   let h = 0
   let i10 = 0
   for (let i = 0; i < len; i++) {
-    let curStar = list[i]
+    let curStar = nums[i]
     sum += curStar
     if (curStar >= i + 1) h = i + 1
     if (curStar >= 10) i10++
   }
-  return {sum, h, i10}
+  return { sum, h, i10 }
 }
 module.exports = hindex
